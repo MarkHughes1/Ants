17c57acdc24a31692ded09aa8b0b65dd6a771e5e
MarkHughes1 <Mark.Hughes4096@gmail.com>
2015-07-05 16:42:07 +0100

    First working copy
    Works but crashed on hitting the screen edge

--- Ants/Form1.Designer.cs
+++ Ants/Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.pct_Output = new System.Windows.Forms.PictureBox();
+            this.btn_go = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pct_Output)).BeginInit();
             this.SuspendLayout();
             // 
@@ -36,20 +37,31 @@
             // 
             this.pct_Output.Location = new System.Drawing.Point(12, 12);
             this.pct_Output.Name = "pct_Output";
-            this.pct_Output.Size = new System.Drawing.Size(268, 249);
+            this.pct_Output.Size = new System.Drawing.Size(568, 514);
             this.pct_Output.TabIndex = 0;
             this.pct_Output.TabStop = false;
             this.pct_Output.Click += new System.EventHandler(this.pct_Output_Click);
+            // 
+            // btn_go
+            // 
+            this.btn_go.Location = new System.Drawing.Point(487, 532);
+            this.btn_go.Name = "btn_go";
+            this.btn_go.Size = new System.Drawing.Size(93, 29);
+            this.btn_go.TabIndex = 1;
+            this.btn_go.Text = "GO!";
+            this.btn_go.UseVisualStyleBackColor = true;
+            this.btn_go.Click += new System.EventHandler(this.btn_go_Click);
             // 
             // frm_Output
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(292, 273);
+            this.ClientSize = new System.Drawing.Size(592, 573);
+            this.Controls.Add(this.btn_go);
             this.Controls.Add(this.pct_Output);
             this.DoubleBuffered = true;
-            this.MaximumSize = new System.Drawing.Size(300, 300);
-            this.MinimumSize = new System.Drawing.Size(300, 300);
+            this.MaximumSize = new System.Drawing.Size(600, 600);
+            this.MinimumSize = new System.Drawing.Size(600, 600);
             this.Name = "frm_Output";
             this.ShowIcon = false;
             this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
@@ -63,6 +75,7 @@
         #endregion
 
         private System.Windows.Forms.PictureBox pct_Output;
+        private System.Windows.Forms.Button btn_go;
     }
 }
 
--- Ants/Form1.cs
+++ Ants/Form1.cs
@@ -24,19 +24,98 @@ namespace Ants
 
         public void frm_Output_Load(object sender, EventArgs e)
         {
-            var myBitmap = new Bitmap(pct_Output.Width, pct_Output.Height);
-            int centerX = Convert.ToInt16( pct_Output.Width * 0.5);
-            int centerY =  Convert.ToInt16(pct_Output.Height * 0.5);
            
+
+        }
+
+        private void btn_go_Click(object sender, EventArgs e)
+        {
+            var myBitmap = new Bitmap(pct_Output.Width, pct_Output.Height);
+            int centerX = Convert.ToInt16(pct_Output.Width * 0.5);
+            int centerY = Convert.ToInt16(pct_Output.Height * 0.5);
+
             var firstAnt = new Ant();
 
             firstAnt.locX = centerX;
             firstAnt.locY = centerY;
+            firstAnt.speedY = 0;
+            firstAnt.speedX = 1;
+            
+            pct_Output.Image = myBitmap;
 
             myBitmap.SetPixel(firstAnt.locX, firstAnt.locY, Color.Black);
 
+            for (int n = 0; n < 20000; n++)
+            {
+                Color pixel = myBitmap.GetPixel(firstAnt.locX, firstAnt.locY);
+
+                if (pixel.R == 0 && pixel.G == 0 && pixel.B == 0)
+                {
+                    //set this pixel white and turn left 
+                    myBitmap.SetPixel(firstAnt.locX, firstAnt.locY, Color.White);
+                    TurnLeft(firstAnt);
+                }
+                else
+                {
+                    //set this pixel black and turn right
+                    myBitmap.SetPixel(firstAnt.locX, firstAnt.locY, Color.Black);
+                    TurnRight(firstAnt);
+                }
+                firstAnt.locX += firstAnt.speedX;
+                firstAnt.locY += firstAnt.speedY;
+            }
+            //myBitmap.SetPixel(MousePosition.X, MousePosition.Y, Color.Black);
+        }
+
+        private void TurnLeft(Ant ant)
+        {
+            if (ant.speedX == 1)
+            {
+                ant.speedX = 0;
+                ant.speedY = -1;
+            }
+            else if (ant.speedX == -1)
+            {
+                ant.speedX = 0;
+                ant.speedY = 1;
+            }
+            else if (ant.speedY == 1)
+            {
+                ant.speedY = 0;
+                ant.speedX = 1;
+            }
+            else if (ant.speedY == -1)
+            {
+                ant.speedY = 0;
+                ant.speedX = -1;
+            }
+        }
+        private void TurnRight(Ant ant)
+        {
+            if (ant.speedX == 1)
+            {
+                ant.speedX = 0;
+                ant.speedY = 1;
+            }
+            else if (ant.speedX == -1)
+            {
+                ant.speedX = 0;
+                ant.speedY = -1;
+            }
+            else if (ant.speedY == 1)
+            {
+                ant.speedY = 0;
+                ant.speedX = -1;
+            }
+            else if (ant.speedY == -1)
+            {
+                ant.speedY = 0;
+                ant.speedX = 1;
+            }
         }
     }
+
+
     public class Ant
     {
         // X and Y current locations
