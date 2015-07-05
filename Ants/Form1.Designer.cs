26c0385f703c782510bb1b5ee3c788de677bde53
MarkHughes1 <Mark.Hughes4096@gmail.com>
2015-07-05 16:08:56 +0100

    Start

---
+++ Ants/Form1.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace Ants
+{
+    partial class frm_Output
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pct_Output = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pct_Output)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pct_Output
+            // 
+            this.pct_Output.Location = new System.Drawing.Point(12, 12);
+            this.pct_Output.Name = "pct_Output";
+            this.pct_Output.Size = new System.Drawing.Size(268, 249);
+            this.pct_Output.TabIndex = 0;
+            this.pct_Output.TabStop = false;
+            this.pct_Output.Click += new System.EventHandler(this.pct_Output_Click);
+            // 
+            // frm_Output
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 273);
+            this.Controls.Add(this.pct_Output);
+            this.DoubleBuffered = true;
+            this.MaximumSize = new System.Drawing.Size(300, 300);
+            this.MinimumSize = new System.Drawing.Size(300, 300);
+            this.Name = "frm_Output";
+            this.ShowIcon = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Langtons Ants";
+            this.Load += new System.EventHandler(this.frm_Output_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pct_Output)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pct_Output;
+    }
+}
+
---
+++ Ants/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ants
+{
+    public partial class frm_Output : Form
+    {
+        public frm_Output()
+        {
+            InitializeComponent();
+        }
+
+        private void pct_Output_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public void frm_Output_Load(object sender, EventArgs e)
+        {
+            var myBitmap = new Bitmap(pct_Output.Width, pct_Output.Height);
+            int centerX = Convert.ToInt16( pct_Output.Width * 0.5);
+            int centerY =  Convert.ToInt16(pct_Output.Height * 0.5);
+           
+            var firstAnt = new Ant();
+
+            firstAnt.locX = centerX;
+            firstAnt.locY = centerY;
+
+            myBitmap.SetPixel(firstAnt.locX, firstAnt.locY, Color.Black);
+
+        }
+    }
+    public class Ant
+    {
+        // X and Y current locations
+        // speed in X and Y (ony one will be either +1 or -1 the other must be 0)
+        public int locX;
+        public int locY;
+
+        public int speedX = 0;
+        public int speedY = 0;
+        
+    }
+}
